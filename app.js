2b7fec1dcdeef217853fc7f29c163a75c2c2608d
Hanz Po <hanznathanpo@gmail.com>
2023-06-12 00:56:12 -0400

    Added Chrome extension files

--- app.js
+++ app.js
@@ -3,6 +3,25 @@ function copyCharacter(character) {
     showToast();
 }
 
+document.addEventListener('DOMContentLoaded', function() {
+    var link = document.getElementById("à");
+    link.addEventListener('click', function() {
+        copyToTheClipboard("à");
+    });
+});
+
+async function copyToTheClipboard(textToCopy){
+    const el = document.createElement('textarea');
+    el.value = textToCopy;
+    el.setAttribute('readonly', '');
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+}
+
 function showToast() {
     var x = document.getElementById("snackbar");
     x.className = "show";
