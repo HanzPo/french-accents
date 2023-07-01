87074b1153df540fdd013c6362bc6e60bbe8a382
Hanz Po <hanznathanpo@gmail.com>
2023-06-30 23:55:00 -0400

    Finish main functionality

---
+++ src/index.js
@@ -0,0 +1,71 @@
+let letter1 = document.getElementById("letter-1")
+let letter2 = document.getElementById("letter-2")
+let letter3 = document.getElementById("letter-3")
+let letter4 = document.getElementById("letter-4")
+let letter5 = document.getElementById("letter-5")
+let letter6 = document.getElementById("letter-6")
+let letter7 = document.getElementById("letter-7")
+let letter8 = document.getElementById("letter-8")
+let letter9 = document.getElementById("letter-9")
+let letter10 = document.getElementById("letter-10")
+let letter11 = document.getElementById("letter-11")
+let letter12 = document.getElementById("letter-12")
+let letter13 = document.getElementById("letter-13")
+let letter14 = document.getElementById("letter-14")
+
+
+letter1.addEventListener("click", () => {
+    navigator.clipboard.writeText("à");
+})
+
+letter2.addEventListener("click", () => {
+    navigator.clipboard.writeText("â");
+})
+
+letter3.addEventListener("click", () => {
+    navigator.clipboard.writeText("ç");
+})
+
+letter4.addEventListener("click", () => {
+    navigator.clipboard.writeText("é");
+})
+
+letter5.addEventListener("click", () => {
+    navigator.clipboard.writeText("è");
+})
+
+letter6.addEventListener("click", () => {
+    navigator.clipboard.writeText("ê");
+})
+
+letter7.addEventListener("click", () => {
+    navigator.clipboard.writeText("ë");
+})
+
+letter8.addEventListener("click", () => {
+    navigator.clipboard.writeText("ï");
+})
+
+letter9.addEventListener("click", () => {
+    navigator.clipboard.writeText("î");
+})
+
+letter10.addEventListener("click", () => {
+    navigator.clipboard.writeText("ô");
+})
+
+letter11.addEventListener("click", () => {
+    navigator.clipboard.writeText("œ");
+})
+
+letter12.addEventListener("click", () => {
+    navigator.clipboard.writeText("ù");
+})
+
+letter13.addEventListener("click", () => {
+    navigator.clipboard.writeText("û");
+})
+
+letter14.addEventListener("click", () => {
+    navigator.clipboard.writeText("ü");
+})
